b4c27f2bac2263adf310104df85e3d5dd38f7f98
Sylvance <9350722+Sylvance@users.noreply.github.com>
2021-12-17 12:48:44 +0300

    [airtel-pesa] Add recursive ostruct

--- lib/airtel/pesa.rb
+++ lib/airtel/pesa.rb
@@ -27,5 +27,13 @@ module Airtel
     def self.configure
       yield(configuration)
     end
+
+    def to_recursive_ostruct(hash)
+      result = hash.each_with_object({}) do |(key, val), memo|
+          memo[key] = val.is_a?(Hash) ? to_recursive_ostruct(val) : val
+      end
+
+      OpenStruct.new(result)
+    end
   end
 end
