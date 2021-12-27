fecc3a210a5a14b35ac721c900827f0649906a85
Sylvance <9350722+Sylvance@users.noreply.github.com>
2021-12-27 21:01:35 +0200

    [airtel-pesa] Add missing require calls

--- lib/airtel/pesa.rb
+++ lib/airtel/pesa.rb
@@ -1,9 +1,21 @@
 # frozen_string_literal: true
-
-require_relative "pesa/authorization"
-require_relative "pesa/encryption"
-require_relative "pesa/version"
 require 'ostruct'
+
+require_relative "pesa/account_balance_enquiry"
+require_relative "pesa/authorization"
+require_relative "pesa/collections_refund_payment"
+require_relative "pesa/collections_transaction_enquiry"
+require_relative "pesa/collections_ussd_push_payment"
+require_relative "pesa/constants"
+require_relative "pesa/disbursement_payment"
+require_relative "pesa/disbursement_transaction_enquiry"
+require_relative "pesa/encryption_pin"
+require_relative "pesa/kyc_user_enquiry"
+require_relative "pesa/remittance_check_eligibility"
+require_relative "pesa/remittance_money_refund"
+require_relative "pesa/remittance_money_transfer_credit"
+require_relative "pesa/remittance_money_transfer_status"
+require_relative "pesa/version"
 
 module Airtel
   module Pesa
